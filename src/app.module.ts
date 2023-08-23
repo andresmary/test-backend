28671d4bcbccb13379cee7089943e862804f080b
Andres Mary <aemary@teco.com.ar>
2023-08-23 17:21:59 -0300

    ajuste

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,15 @@ import { Module } from '@nestjs/common';
 // const envModule = ConfigModule.forRoot({
 //   isGlobal: true,
 // });
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeormConnectionConfig } from './config/typeorm.config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { typeormConnectionConfig } from './config/typeorm.config';
 import { ProductsModule } from './products/products.module';
 import { TablesModule } from './tables/tables.module';
 
 @Module({
   imports: [
     // envModule,
-    TypeOrmModule.forRoot(typeormConnectionConfig),
+    // TypeOrmModule.forRoot(typeormConnectionConfig),
     ProductsModule,
     TablesModule,
   ],
