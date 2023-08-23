7ef23ff47a604187ac7e171704766230e8b226de
Andres Mary <aemary@teco.com.ar>
2023-08-23 14:38:24 -0300

    test source

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,14 @@ import { Module } from '@nestjs/common';
 //   isGlobal: true,
 // });
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { typeormConnectionConfig } from './config/typeorm.config';
+import { typeormConnectionConfig } from '../src/config/typeorm.config';
 import { ProductsModule } from './products/products.module';
 import { TablesModule } from './tables/tables.module';
 
 @Module({
   imports: [
     // envModule,
-    // TypeOrmModule.forRoot(typeormConnectionConfig),
+    TypeOrmModule.forRoot(typeormConnectionConfig),
     ProductsModule,
     TablesModule,
   ],
